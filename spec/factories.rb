aa92c6c8b68c317022897d500920dff4e6a29d55
Dawid Warpas <dawid.warpas@gmail.com>
2014-02-03 17:07:07 +0100

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dawid Warpas"
+    email    "dawid.warpas@gmail.com"
+    password "mieczz12"
+    password_confirmation "mieczz12"
+  end
+end
